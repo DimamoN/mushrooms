cf0e74e861da10a8f984255547327e5fc410be08
dimamon <dimamon95@gmail.com>
2019-01-06 12:19:53 +0200

    notifications and health added

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 
-let intro, home, forest, city,
+let intro, home, forest, city, notification,
 
-    info,
+    info, ntfTitle, ntfText,
 
     btnStart, btnHomeToForest, btnHomeToCity,
     btnForestToHome, btnCityToHome, btnSleep,
-    btnPickMushrooms, btnSellMushrooms;
+    btnPickMushrooms, btnSellMushrooms,
+    btnNtfOk;
 
 $(document).ready(function () {
 
@@ -14,9 +15,12 @@ $(document).ready(function () {
     home = $("#home")[0];
     forest = $("#forest")[0];
     city = $("#city")[0];
+    notification = $("#notification")[0];
 
     // text //
     info = $("#info")[0];
+    ntfText = $("#ntf-text")[0];
+    ntfTitle = $("#ntf-title")[0];
 
     // buttons //
     btnStart = $("#start")[0];
@@ -28,6 +32,7 @@ $(document).ready(function () {
     btnSleep = $("#sleep")[0];
     btnPickMushrooms = $("#pick-mushrooms")[0];
     btnSellMushrooms = $("#sell-mushrooms")[0];
+    btnNtfOk = $("#ntf-ok")[0];
 
     btnStart.onclick = () => startGame();
     btnHomeToCity.onclick = () => game.goHomeToCity();
@@ -37,6 +42,7 @@ $(document).ready(function () {
     btnSleep.onclick = () => game.sleep();
     btnPickMushrooms.onclick = () => game.pickMushrooms();
     btnSellMushrooms.onclick = () => game.sellMushrooms();
+    btnNtfOk.onclick = () => ui.closeNotification();
 });
 
 const game = {
@@ -48,48 +54,64 @@ const game = {
     CITY_TEXT : "You are in the city",
     FOREST_TEXT : "You are in the forest",
 
+    NO_MUSHROOMS : "You have no mushrooms to sell",
+    NO_ENERGY_TEXT : "You are very tired",
+
+    NO_ENERGY_TRAVEL_TEXT : "You traveled tired. Your health has suffered",
+
     sleep : function () {
         stats.updDay();
+        stats.refreshEnergy();
         ui.renderStats();
     },
 
     pickMushrooms : function () {
-        stats.updMushrooms();
+        if (stats.hasEnergy()) {
+            stats.updMushrooms();
+            stats.minusEnergy();
+        } else {
+            ui.showNotification(game.NO_ENERGY_TEXT);
+        }
         ui.renderStats();
     },
 
     sellMushrooms : function () {
+        if (stats.mushrooms === 0) {
+            ui.showNotification(game.NO_MUSHROOMS);
+            return;
+        }
         stats.sellMushrooms();
         ui.renderStats();
     },
 
-    goHomeToCity : function () {
-        ui.show(city);
-        ui.hide(home);
-        stats.setPlace(game.CITY_TEXT);
+    travel : function (from, to, toText) {
+        if (!stats.hasEnergy()) {
+            ui.showNotification(game.NO_ENERGY_TRAVEL_TEXT);
+            stats.minusHealth();
+        } else {
+            stats.minusEnergy();
+        }
+        ui.show(to);
+        ui.hide(from);
+        stats.setPlace(toText);
         ui.renderStats();
+    },
+
+    goHomeToCity : function () {
+        this.travel(home, city, game.CITY_TEXT);
     },
     
     goHomeToForest : function () {
-        ui.show(forest);
-        ui.hide(home);
-        stats.setPlace(game.FOREST_TEXT);
-        ui.renderStats();
+        this.travel(home, forest, game.FOREST_TEXT);
     },
     
     goForestToHome : function () {
-        ui.show(home);
-        ui.hide(forest);
-        stats.setPlace(game.HOME_TEXT);
-        ui.renderStats();
+        this.travel(forest, home, game.HOME_TEXT);
     },
     
     goCityToHome : function ( ) {
-        ui.show(home);
-        ui.hide(city);
-        stats.setPlace(game.HOME_TEXT);
-        ui.renderStats();
-    }
+        this.travel(city, home, game.HOME_TEXT);
+    },
 };
 
 const stats = {
@@ -97,11 +119,24 @@ const stats = {
     day : 1,
     mushrooms : 0,
     money : 0,
-    energy : 100,
+    energy : 10,
+    health : 10,
     place : "",
 
     updMushrooms: function () {
         return this.mushrooms++;
+    },
+
+    hasEnergy: function () {
+        return this.energy > 0;
+    },
+
+    minusEnergy: function () {
+        this.energy--;
+    },
+
+    minusHealth: function () {
+        this.health--;
     },
 
     sellMushrooms: function () {
@@ -109,21 +144,25 @@ const stats = {
         this.mushrooms = 0;
     },
 
+    refreshEnergy: function () {
+        this.energy = 10;
+    },
+
     updDay: function () {
         this.day++;
         
         switch (this.day) {
-            case 4 : alert("Today you have a good mood"); break;
-            case 6 : alert("It is rainy today, that's mean that tomorrow will be a lot of mushrooms"); break;
-            case 10 : alert("You have heavy headache today, can't go to the forest"); break;
+            case 4 : ui.showNotification("Today you have a good mood"); break;
+            case 6 : ui.showNotification("It is rainy today, that's mean that tomorrow will be a lot of mushrooms"); break;
+            case 10 : ui.showNotification("You have heavy headache today, can't go to the forest"); break;
             case 40 : {
-                alert("You get sick");
+                ui.showNotification("You get sick");
             } break;
             case 45 : {
-                alert("You are getting very bad");
+                ui.showNotification("You are getting very bad");
             } break;
             case 50 : {
-                alert("You died");
+                ui.showNotification("You died");
                 window.location.reload();
             } break;
         }
@@ -148,12 +187,21 @@ const ui = {
 
     renderStats : function() {
         info.innerHTML = stats.place + "<br>" +
-        "day : " + stats.day + "<br>" +
+        "day : " + stats.day + "<br><br>" +
         "mushrooms : " + stats.mushrooms + "<br>" +
         "money : " + stats.money + "<br>" +
+        "health: " + stats.health + "<br>" +
         "energy : " + stats.energy;
-}
+    },
 
+    closeNotification : function () {
+        this.hide(notification);
+    },
+
+    showNotification : function (text) {
+        ntfText.innerHTML = text;
+        this.show(notification);
+    }
 };
 
 function startGame() {
